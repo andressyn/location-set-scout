fe91fac8f34d25431e6989ca20cc3417c3c9a6ba
andresd7 <andres@synergicsoftware.com>
2026-01-18 23:37:51 -0500

    update

--- site.config.ts
+++ site.config.ts
@@ -1,5 +1,5 @@
-export const siteUrl = "https://lookoutscout.vercel.app";
-export const siteTitle = "Lookout Set Scout";
+export const siteUrl = "https://locationscout.vercel.app";
+export const siteTitle = "Lookout Scout";
 export const defaultLocale = "en";
 export const locales = ["en", "fr"];
 export type Locale = "en" | "fr";
